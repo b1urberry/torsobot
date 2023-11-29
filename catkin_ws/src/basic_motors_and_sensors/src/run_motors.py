b43577ff50f6c28425d43dc0775bc8bdcb4b782a
blurberry <48646019+dengden@users.noreply.github.com>
2023-11-29 14:35:12 -0600

    removed useless files

--- catkin_ws/src/basic_motors_and_sensors/src/run_motors.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-
-from pololu_drv8835_rpi import motors
-print("issue the commands")
-motors.motor1.setSpeed(300) #150
-motors.motor2.setSpeed(300) #150
-print("finish the commands")
